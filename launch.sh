636cd1e8bc41f8397f7f89c051e4ba2c1b642e3f
edwardxie <edwardxie1@gmail.com>
2020-12-02 13:05:15 +0000

    2020-12-02

--- launch.sh
+++ launch.sh
@@ -1,3 +1,2 @@
 #1/bin/sh
-nohup java -Xms2048m -Xmx4096m -jar fabric-server-launch.jar -nogui > server.out
-nohup python backup.py > backup.out
+nohup java -Xms2048m -Xmx4096m -jar fabric-server-launch.jar -nogui > server.out &nohup python backup.py > backup.out &
